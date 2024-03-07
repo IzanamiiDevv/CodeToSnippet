3c78eb8386c0d84df1af07c5c3e0a6712c8be25c
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-07 21:59:30 +0800

    code

---
+++ main.js
@@ -0,0 +1,10 @@
+function compileToSnippet(prefix,description,script) {
+    const lines = script.split('\n');
+    const snippet = {
+        'prefix': prefix,
+        'body': lines,
+        'description': description
+    };
+
+    return JSON.stringify(snippet, null, 2);
+}
