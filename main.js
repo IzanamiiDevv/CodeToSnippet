301d0c6c6e8ffa414a746f8218d365c6d5a8a026
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-09 18:38:12 +0800

    Omsim

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
   const copyButton = document.getElementById('copyButton');
 
   copyButton.addEventListener('click', function() {
+    const prefix = document.getElementById('pre').value;
+    const description = document.getElementById('desc').value;
     window.location.href = '#Editor';
     // Get the current value from the editor
     const editorValue = document.getElementById('editor').innerText;
 
     // Create a temporary textarea element to hold the text
     const textarea = document.createElement('textarea');
-    textarea.value = compileToSnippet(null, null, editorValue);
+    textarea.value = compileToSnippet(prefix, description, editorValue);
 
     // Append the textarea to the document
     document.body.appendChild(textarea);
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 
+console.log('  CodeToSnippet:')
+
 export default defineConfig({
     server: {
         port: 3000
