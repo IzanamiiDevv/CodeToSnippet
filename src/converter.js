22ae8e259293b92b8f6cd42bde6cb614088ebd17
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-09 16:22:13 +0800

    comment

--- src/converter.js
+++ src/converter.js
@@ -1,12 +1,18 @@
 function compileToSnippet(prefix,description,script) {
+    // Split the String For Every '\n' or line
     const lines = script.split('\n');
+
+    // Snippet Template
     const snippet = {
         'prefix': prefix,
         'body': lines,
         'description': description
     };
 
+    // Returning the Snippet
     return JSON.stringify(snippet, null, 2);
 }
 
+
+// Export The Convert Method
 export default compileToSnippet;
