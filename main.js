68d5d11d30fa205b133893ca221fb4402749030a
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-09 11:51:55 +0800

    ss

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import compileToSnippet from './src/converter';
 
-
+document.getElementById('editor').addEventListener('input',()=>{
+    console.log(compileToSnippet(null,null,document.getElementById('editor').innerText))
+})
